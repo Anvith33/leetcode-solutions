28bbc162c197b486cf613813f7281fa316224e00
Anvith C L <anvithchakote33@gmail.com>
2026-07-12 13:47:18 +0530

    Time: 24 ms (50.49%), Space: 90.7 MB (38.22%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> lastIndex = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (lastIndex.containsKey(nums[i])) {
+                int prevIndex = lastIndex.get(nums[i]);
+                if (i - prevIndex <= k) {
+                    return true;
+                }
+            }
+            lastIndex.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}
