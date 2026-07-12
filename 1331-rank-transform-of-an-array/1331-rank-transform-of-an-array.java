598cd8581d950506e6f5c99fddd03e9f51871d18
Anvith C L <anvithchakote33@gmail.com>
2026-07-12 21:38:31 +0530

    Time: 29 ms (84.49%), Space: 75.3 MB (69.26%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,23 +1,25 @@
 import java.util.*;
-class Solution{
-    public int[] arrayRankTransform(int[] arr){
-        int n=arr.length;
-        if(n==0) return new int[0];
-        int[] sorted=arr.clone();
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        if (arr == null || arr.length == 0) return new int[0];
+
+        int n = arr.length;
+        int[] sorted = Arrays.copyOf(arr, n);
         Arrays.sort(sorted);
 
-        Map<Integer,Integer> rankMap= new HashMap<>();
-        int rank=1;
-        for(int num:sorted){
-            if(!rankMap.containsKey(num)){
-                rankMap.put(num,rank);
-                rank++;
+        Map<Integer, Integer> rank = new HashMap<>();
+        int r = 1;
+        for (int v : sorted) {
+            if (!rank.containsKey(v)) {
+                rank.put(v, r++);
             }
         }
-        int[] result=new int[n];
-        for(int i=0;i<n;i++) {
-            result[i]=rankMap.get(arr[i]);
+
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = rank.get(arr[i]);
         }
-        return result;
+        return res;
     }
 }
