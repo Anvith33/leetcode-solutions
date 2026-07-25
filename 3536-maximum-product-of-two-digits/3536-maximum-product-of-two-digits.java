a06c9b5db1afc0c526fb5550faf6dcca5a83b6e2
Anvith C L <anvithchakote33@gmail.com>
2026-07-25 20:32:34 +0530

    Time: 1 ms (100%), Space: 42.9 MB (11.19%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxProduct(int n) {
         // Extract digits
-        int[] digits = new int[10]; // frequency of digits 0-9
+        int[] digits = new int[10];
         while (n > 0) {
             digits[n % 10]++;
             n /= 10;
         }
 
-        // Find the two largest digits
         int first = -1, second = -1;
         for (int d = 9; d >= 0; d--) {
             if (digits[d] > 0) {
                 if (first == -1) {
                     first = d;
-                    // If digit appears more than once, it can be used twice
+                  
                     if (digits[d] > 1) {
                         second = d;
                         break;
