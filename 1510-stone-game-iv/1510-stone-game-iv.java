5684707a8f5a7bb99078dcc2bfe199399f288240
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 00:43:27 +0530

    Time: 14 ms (91.38%), Space: 42.2 MB (96.34%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean[] dp = new boolean[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                int square = j * j;
+
+                // If after our move the opponent is in
+                // a losing position, we can win.
+                if (!dp[i - square]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
