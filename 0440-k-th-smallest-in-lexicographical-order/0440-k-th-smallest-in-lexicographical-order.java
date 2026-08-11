0df3ccef6ea844ebe46af474a77b3c62570bd1c2
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 23:45:54 +0530

    Time: 0 ms (100%), Space: 42 MB (76.06%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -1,37 +1,26 @@
 class Solution {
-    public int findKthNumber(int n, int k) {
-        long curr = 1;
-        k--; 
-        
-        while (k > 0) {
-            long count = countSteps(n, curr, curr + 1);
-            
-            if (count <= k) {
+    private int getReqNum(long a, long b, long n) {
+        int gap = 0;
+        while (a <= n) {
+            gap += Math.min(n + 1, b) - a;
+            a *= 10;
+            b *= 10;
+        }
+        return gap;
+    }
 
-                curr++;
-                k -= count;
+    public int findKthNumber(int n, int k) {
+        long num = 1;
+        for (int i = 1; i < k;) {
+            int req = getReqNum(num, num + 1, n);
+            if (i + req <= k) {
+                i += req;
+                num++;
             } else {
-          
-                curr *= 10;
-                k--;
+                i++;
+                num *= 10;
             }
         }
-        
-        return (int) curr;
-    }
-    
-  
-    private long countSteps(int n, long prefix1, long prefix2) {
-        long count = 0;
-        long first = prefix1;
-        long last = prefix2;
-        
-        while (first <= n) {
-            count += Math.min(n + 1, last) - first;
-            first *= 10;
-            last *= 10;
-        }
-        
-        return count;
+        return (int) num;
     }
 }
