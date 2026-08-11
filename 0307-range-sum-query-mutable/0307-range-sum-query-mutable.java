e0b141676c7ce7df10834c4601cc3c9021a9ef54
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 23:42:30 +0530

    Time: 84 ms (62.54%), Space: 134.5 MB (37.02%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -1,36 +1,45 @@
-class NumArray {
-    private int[] tree;
-    private int[] nums;
-    private int n;
-    
-    public NumArray(int[] nums) {
-        this.n = nums.length;
-        this.nums = new int[n];
-        this.tree = new int[n + 1];
-        
-        for (int i = 0; i < n; i++) {
-            update(i, nums[i]);
-        }
-    }
-    
-    public void update(int index, int val) {
-        int delta = val - nums[index];
-        nums[index] = val;
-        
-        for (int i = index + 1; i <= n; i += i & (-i)) {
-            tree[i] += delta;
-        }
-    }
-    
-    public int sumRange(int left, int right) {
-        return prefixSum(right + 1) - prefixSum(left);
-    }
-    
-    private int prefixSum(int i) {
-        int sum = 0;
-        for (; i > 0; i -= i & (-i)) {
-            sum += tree[i];
-        }
-        return sum;
-    }
+public class NumArray {
+
+
+	int[] nums;
+	int[] BIT;
+	int n;
+
+	public NumArray(int[] nums) {
+		this.nums = nums;
+
+		n = nums.length;
+		BIT = new int[n + 1];
+		for (int i = 0; i < n; i++)
+			init(i, nums[i]);
+	}
+
+	public void init(int i, int val) {
+		i++;
+		while (i <= n) {
+			BIT[i] += val;
+			i += (i & -i);
+		}
+	}
+
+	void update(int i, int val) {
+		int diff = val - nums[i];
+		nums[i] = val;
+		init(i, diff);
+	}
+
+	public int getSum(int i) {
+		int sum = 0;
+		i++;
+		while (i > 0) {
+			sum += BIT[i];
+			i -= (i & -i);
+		}
+		return sum;
+	}
+
+	public int sumRange(int i, int j) {
+		return getSum(j) - getSum(i - 1);
+	}
 }
+
