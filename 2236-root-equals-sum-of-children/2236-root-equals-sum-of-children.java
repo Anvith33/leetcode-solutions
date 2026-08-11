ce0a68d5e370af8e45fd720aaf16ef5d7de6a60d
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 23:37:15 +0530

    Time: 0 ms (100%), Space: 43.2 MB (13.31%) - LeetHub

--- 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
+++ 2236-root-equals-sum-of-children/2236-root-equals-sum-of-children.java
@@ -1,5 +1,5 @@
 class Solution {
     public boolean checkTree(TreeNode root) {
-        return root.val == root.left.val + root.right.val;
+       return root.val == root.right.val + root.left.val; 
     }
 }
