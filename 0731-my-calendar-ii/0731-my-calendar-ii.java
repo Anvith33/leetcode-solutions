d56769eafbab82676dcaa22519eb1bb82a6840c9
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 23:44:20 +0530

    Time: 45 ms (90.58%), Space: 47.2 MB (79.4%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,32 +1,51 @@
-import java.util.*;
+class Event
+{
+  int start;
+  int end;
+  public Event(int start, int end)
+  {
+    this.start = start;
+    this.end = end;
+  }  
+}
 
 class MyCalendarTwo {
-    private List<int[]> bookings;
-    private List<int[]> overlaps;
-    
+    List<Event> overLapBookings;
+    List<Event> bookings;
+
     public MyCalendarTwo() {
+        overLapBookings = new ArrayList<>();
         bookings = new ArrayList<>();
-        overlaps = new ArrayList<>();
     }
     
-    public boolean book(int startTime, int endTime) {
-        for (int[] overlap : overlaps) {
-            if (startTime < overlap[1] && endTime > overlap[0]) {
-   
-                return false;
-            }
-        }
+    public boolean book(int start, int end) {
         
-        for (int[] booking : bookings) {
-            int overlapStart = Math.max(startTime, booking[0]);
-            int overlapEnd = Math.min(endTime, booking[1]);
-            if (overlapStart < overlapEnd) {
-                overlaps.add(new int[]{overlapStart, overlapEnd});
-            }
-        }
-        
-        bookings.add(new int[]{startTime, endTime});
-        return true;
+    for(Event e : overLapBookings)
+    {
+      if(isOverlap(e, start, end))
+      return false;  
+    } 
+
+    for(Event e : bookings)
+    {
+      if(isOverlap(e, start, end))
+      {
+        overLapBookings.add(getOverlapEvent(e, start, end));
+      }    
+    }  
+       bookings.add(new Event(start, end));
+
+       return true; 
+    }
+
+    public boolean isOverlap(Event e, int start, int end)
+    {
+      return Math.max(e.start, start)<Math.min(e.end, end); 
+    }
+
+    public Event getOverlapEvent(Event e, int start, int end)
+    {
+      return new Event(Math.max(e.start, start), Math.min(e.end, end));  
     }
 }
 
