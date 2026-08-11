ccd9bbbee74e80b80ea7cfaf8dcfb17bc0b1336b
Anvith C L <anvithchakote33@gmail.com>
2026-08-12 00:03:47 +0530

    Time: 20 ms (86.19%), Space: 45.5 MB (32.79%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.java
@@ -1,50 +1,36 @@
-import java.util.*;
-
 class SummaryRanges {
-    private TreeMap<Integer, Integer> intervals; 
-    
-    public SummaryRanges() {
-        intervals = new TreeMap<>();
-    }
+
+    public SummaryRanges() {  }
+    TreeSet<Integer> set = new TreeSet<>();
     
     public void addNum(int value) {
-       
-        Map.Entry<Integer, Integer> floorEntry = intervals.floorEntry(value);
-        if (floorEntry != null && floorEntry.getValue() >= value) {
-            return; 
-        }
-        
-        boolean mergeWithLeft = floorEntry != null && floorEntry.getValue() == value - 1;
-        
-        Map.Entry<Integer, Integer> ceilingEntry = intervals.ceilingEntry(value);
-        boolean mergeWithRight = ceilingEntry != null && ceilingEntry.getKey() == value + 1;
-        
-        if (mergeWithLeft && mergeWithRight) {
-           
-            intervals.put(floorEntry.getKey(), ceilingEntry.getValue());
-            intervals.remove(ceilingEntry.getKey());
-        } else if (mergeWithLeft) {
-       
-            intervals.put(floorEntry.getKey(), value);
-        } else if (mergeWithRight) {
-            
-            intervals.put(value, ceilingEntry.getValue());
-            intervals.remove(ceilingEntry.getKey());
-        } else {
-       
-            intervals.put(value, value);
-        }
+        set.add(value);      
     }
     
     public int[][] getIntervals() {
-        int[][] result = new int[intervals.size()][2];
-        int i = 0;
-        for (Map.Entry<Integer, Integer> entry : intervals.entrySet()) {
-            result[i][0] = entry.getKey();
-            result[i][1] = entry.getValue();
-            i++;
+        ArrayList<int[]> al = new ArrayList<>();
+        if (set.isEmpty()) return new int[0][];
+        int start = -1,end = -1;
+        for(int i : set){
+            if(start == -1 && end == -1)
+                start = end = i;
+            else if(start != -1 && i == end + 1)
+                end = i;
+            else if(i > end + 1){
+                al.add(new int[]{start, end});
+                start = end = i;
+            }
         }
-        return result;
+
+        al.add(new int[]{start, end});
+
+        int[][] arr = new int[al.size()][2];
+        for(int i=0; i<al.size(); i++){
+            arr[i] = al.get(i);
+        }
+
+        return arr;
+
+        
     }
 }
-
