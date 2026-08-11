80dce4dd3b4dab1f9dad77b3008cae6af636b88c
Anvith C L <anvithchakote33@gmail.com>
2026-08-12 00:00:02 +0530

    Time: 54 ms (65.37%), Space: 134.8 MB (86.64%) - LeetHub

--- 0460-lfu-cache/0460-lfu-cache.java
+++ 0460-lfu-cache/0460-lfu-cache.java
@@ -1,74 +1,110 @@
 import java.util.*;
 
 class LFUCache {
+
     class Node {
         int key, value, freq;
-        Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-            this.freq = 1;
+        Node prev, next;
+
+        Node(int k, int v) {
+            key = k;
+            value = v;
+            freq = 1;
         }
     }
-    
-    private int capacity;
-    private int minFreq;
-    private Map<Integer, Node> keyToNode;
-    private Map<Integer, LinkedHashSet<Integer>> freqToKeys;
-    
+
+    class DLL {
+        Node head = new Node(0,0);
+        Node tail = new Node(0,0);
+        int size = 0;
+
+        DLL() {
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        void add(Node node) {
+            node.prev = head;
+            node.next = head.next;
+            head.next.prev = node;
+            head.next = node;
+            size++;
+        }
+
+        void remove(Node node) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+
+        Node removeLast() {
+            if(size > 0) {
+                Node node = tail.prev;
+                remove(node);
+                return node;
+            }
+            return null;
+        }
+    }
+
+    Map<Integer, Node> keyMap = new HashMap<>();
+    Map<Integer, DLL> freqMap = new HashMap<>();
+
+    int capacity;
+    int minFreq;
+
     public LFUCache(int capacity) {
         this.capacity = capacity;
-        this.minFreq = 0;
-        keyToNode = new HashMap<>();
-        freqToKeys = new HashMap<>();
     }
-    
+
     public int get(int key) {
-        if (!keyToNode.containsKey(key)) {
-            return -1;
-        }
-        
-        Node node = keyToNode.get(key);
-        updateFreq(node);
+        if(!keyMap.containsKey(key)) return -1;
+
+        Node node = keyMap.get(key);
+        update(node);
         return node.value;
     }
-    
+
     public void put(int key, int value) {
-        if (capacity == 0) {
-            return;
-        }
-        
-        if (keyToNode.containsKey(key)) {
-            Node node = keyToNode.get(key);
+
+        if(capacity == 0) return;
+
+        if(keyMap.containsKey(key)) {
+            Node node = keyMap.get(key);
             node.value = value;
-            updateFreq(node);
+            update(node);
             return;
         }
-        
-        if (keyToNode.size() == capacity) {
-            LinkedHashSet<Integer> minFreqKeys = freqToKeys.get(minFreq);
-            int evictKey = minFreqKeys.iterator().next();
-            minFreqKeys.remove(evictKey);
-            keyToNode.remove(evictKey);
+
+        if(keyMap.size() == capacity) {
+            DLL list = freqMap.get(minFreq);
+            Node removed = list.removeLast();
+            keyMap.remove(removed.key);
         }
-        
-        Node newNode = new Node(key, value);
-        keyToNode.put(key, newNode);
-        freqToKeys.computeIfAbsent(1, k -> new LinkedHashSet<>()).add(key);
+
+        Node node = new Node(key,value);
+        keyMap.put(key,node);
+
         minFreq = 1;
+
+        freqMap.putIfAbsent(1,new DLL());
+        freqMap.get(1).add(node);
     }
-    
-    private void updateFreq(Node node) {
-        int oldFreq = node.freq;
-        freqToKeys.get(oldFreq).remove(node.key);
-        
-        if (freqToKeys.get(oldFreq).isEmpty()) {
-            freqToKeys.remove(oldFreq);
-            if (minFreq == oldFreq) {
-                minFreq++;
-            }
+
+    private void update(Node node) {
+
+        int freq = node.freq;
+        DLL list = freqMap.get(freq);
+
+        list.remove(node);
+
+        if(freq == minFreq && list.size == 0) {
+            minFreq++;
         }
-        
+
         node.freq++;
-        freqToKeys.computeIfAbsent(node.freq, k -> new LinkedHashSet<>()).add(node.key);
+
+        freqMap.putIfAbsent(node.freq,new DLL());
+        freqMap.get(node.freq).add(node);
     }
 }
