f5e6efe0babde2c552b663cd6d884070eb1396df
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 23:39:18 +0530

    Time: 71 ms (42.19%), Space: 86 MB (49.9%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.java
@@ -1,36 +1,27 @@
-import java.util.*;
-
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-
-        HashMap<Integer, TreeNode> map = new HashMap<>();
-        HashSet<Integer> childSet = new HashSet<>();
-
-        for (int[] d : descriptions) {
-
-            int parent = d[0];
-            int child = d[1];
-            int isLeft = d[2];
-
-            map.putIfAbsent(parent, new TreeNode(parent));
-            map.putIfAbsent(child, new TreeNode(child));
-
-            if (isLeft == 1) {
-                map.get(parent).left = map.get(child);
-            } else {
-                map.get(parent).right = map.get(child);
-            }
-
-            childSet.add(child);
+        Map<Integer, TreeNode> mp = new HashMap<>();
+        Set<Integer> hasParent = new HashSet<>();
+        
+        for (int i = 0; i < descriptions.length; i++) {
+            if (!mp.containsKey(descriptions[i][0]))
+                mp.put(descriptions[i][0], new TreeNode(descriptions[i][0]));
+            if (!mp.containsKey(descriptions[i][1]))
+                mp.put(descriptions[i][1], new TreeNode(descriptions[i][1]));
+            hasParent.add(descriptions[i][1]);
         }
-
-        for (int[] d : descriptions) {
-            int parent = d[0];
-            if (!childSet.contains(parent)) {
-                return map.get(parent);
+        
+        TreeNode root = null;
+        for (int i = 0; i < descriptions.length; i++) {
+            if (descriptions[i][2] == 1) { // left
+                mp.get(descriptions[i][0]).left = mp.get(descriptions[i][1]);
+            } else { // right
+                mp.get(descriptions[i][0]).right = mp.get(descriptions[i][1]);
+            }
+            if (!hasParent.contains(descriptions[i][0])) {
+                root = mp.get(descriptions[i][0]);
             }
         }
-
-        return null;
+        return root;
     }
 }
