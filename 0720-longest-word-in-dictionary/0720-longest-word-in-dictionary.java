b813f2c23195d378bae0fa19696a8c6db2e3d17a
Anvith C L <anvithchakote33@gmail.com>
2026-08-11 23:45:24 +0530

    Time: 17 ms (50.13%), Space: 46.5 MB (76.34%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -2,28 +2,23 @@ import java.util.*;
 
 class Solution {
     public String longestWord(String[] words) {
-        Arrays.sort(words, (a, b) -> {
-            if (a.length() != b.length()) {
-                return a.length() - b.length();
-            }
-            return a.compareTo(b);
-        });
+        Arrays.sort(words);
         
-        Set<String> buildable = new HashSet<>();
-        buildable.add(""); 
-        
-        String result = "";
+        Set<String> st = new HashSet<>();
+        st.add("");
+        String longest = "";
         
         for (String word : words) {
             String prefix = word.substring(0, word.length() - 1);
-            if (buildable.contains(prefix)) {
-                buildable.add(word);
-                if (word.length() > result.length()) {
-                    result = word;
+            
+            if (st.contains(prefix)) {
+                st.add(word);
+                if (word.length() > longest.length()) {
+                    longest = word;
                 }
             }
         }
         
-        return result;
+        return longest;
     }
 }
